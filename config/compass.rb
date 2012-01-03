be171427406ba205883ffec039cb35772bc0ffb0
Philip (flip) Kromer <flip@infochimps.org>
2012-01-02 23:45:00 -0600

    got bootstrap + sass working

---
+++ config/compass.rb
@@ -0,0 +1,8 @@
+project_type     = :rails
+project_path     = Compass::AppIntegration::Rails.root
+http_path        = "/"
+css_dir          = "tmp/stylesheets"
+sass_dir         = "app/assets/stylesheets"
+javascripts_dir  = "app/assets/javascripts"
+images_dir       = "app/assets/images"
+preferred_syntax = :sass
