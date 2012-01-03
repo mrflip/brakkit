e751f5d56bb9f6a7ab4d6c9c80059d243d6b6e22
Philip (flip) Kromer <flip@infochimps.org>
2012-01-02 23:19:30 -0600

    Generated rspec install.

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,11 @@ end
 
 module Brak
   class Application < Rails::Application
+      config.generators do |g|
+      g.test_framework   :rspec, :fixture => true, :views => false
+      g.integration_tool :rspec, :fixture => true, :views => true
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
