ef42bb2cb97c842e52c0a10b057e73a2613729f3
Philip (flip) Kromer <flip@infochimps.org>
2012-01-02 23:19:25 -0600

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Brak::Application.config.secret_token = '0419b99a96b768dcf88e35111dca78b4121d9c6480b23ffe6a60faaa1a97a3a2f94ada3be58063685b5b4f2b3bde9e7b51a2ca0212c4de434a0df8df0609cee6'
