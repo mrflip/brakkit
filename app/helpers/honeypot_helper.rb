082c9c0d013f3b73eac1508a02599764671b2fcf
Philip (flip) Kromer <flip@infochimps.org>
2012-01-14 20:32:27 -0600

    added hidden honeypot field helpers

---
+++ app/helpers/honeypot_helper.rb
@@ -0,0 +1,9 @@
+module HoneypotHelper
+  def honeypot_tag
+    content_tag(:div, :class => 'honey') do
+      str  = content_tag(:span, 'Only fill in if you are an evil robot')
+      str << text_field_tag("honey_#{rand(1024).to_s(36)}", 'asdf', :tabindex => -1)
+      str
+    end
+  end
+end
